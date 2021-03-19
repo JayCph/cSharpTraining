79bca467e2c7fd474d0953d88feb7132e5c4194a
Janus B. Reedtz <janus.reedtz@gmail.com>
2021-03-19 18:59:03 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _15_passwordValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Code about some passwords etc coming here
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
